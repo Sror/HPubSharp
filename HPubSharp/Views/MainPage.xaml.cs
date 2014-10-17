b713fa9f332bbb3f3280f0acc90ec137f397d9b3
Todd Henderson <todd@todd-henderson.me>
2014-10-17 18:36:49 -0500

    Initial commit with project skeleton.

---
+++ HPubSharp/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HPubSharp
+{	
+	public partial class MainPage : ContentPage
+	{	
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
