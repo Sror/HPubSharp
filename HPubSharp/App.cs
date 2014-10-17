40311f47a586647d4ed7cf56705b3f6d083130da
Todd Henderson <todd@todd-henderson.me>
2014-10-17 18:43:47 -0500

    Adding the rest of the files.

---
+++ HPubSharp/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HPubSharp
+{
+	/// <summary>
+	/// The main class in the PCL.
+	/// </summary>
+	public class App
+	{
+		/// <summary>
+		/// Returns the main view controller generated from the xaml.
+		/// </summary>
+		/// <returns>The main page view.</returns>
+		public static Page GetMainPage ()
+		{
+			return new MainPage ();
+		}
+	}
+}
+
---
+++ HPubSharp/IBook.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HPubSharp
+{
+	public interface IBook
+	{
+
+	}
+}
+
